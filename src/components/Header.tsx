3605ae7e7383bfa458e585c7d0dcb81f666b248e
desrtCodr <ian.mitchard@gmail.com>
2023-05-10 10:42:52 -0600

    added logo responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,37 +14,58 @@ const Header = () => {
   });
   return (
     <header className="flex items-center justify-between gap-2 p-3 xs:flex-col sm:flex-row">
-      <div className="flex w-full flex-col gap-1 px-2">
-        <div className="flex items-center gap-2 xs:max-sm:justify-between">
-          <h1 className="flex items-center gap-3 text-2xl font-bold text-gray-900">
-            Task Manager
-          </h1>
-          <p className="text-md mt-1 font-medium text-gray-500">
-            -- Get Your Life Together --
-          </p>
+      <div className="flex justify-between gap-1 px-2 xs:max-md:w-full md:flex-col">
+        <Image
+          className="flex items-center justify-center p-2 md:hidden"
+          src="/favicon.ico"
+          width={70}
+          height={40}
+          alt="Logo"
+        />
+        <div className="flex ">
+          <div>
+            <div className="flex items-center gap-2 xs:max-sm:justify-between">
+              <h1 className="flex items-center gap-3 text-2xl font-bold text-gray-900">
+                Task Manager
+              </h1>
+              <p className="text-md mt-1 font-medium text-gray-500">
+                -- Get Your Life Together --
+              </p>
+            </div>
+
+            <form className="flex gap-2 xs:max-sm:justify-between">
+              <input
+                placeholder="Enter New Project"
+                className="flex border px-2 py-1"
+                type="text"
+                value={newProject}
+                onChange={(e) => setNewProject(e.target.value)}
+              />
+              <button
+                className="flex w-32 rounded-lg border border-cyan-600 px-2 py-1 text-center font-semibold text-gray-500 no-underline transition hover:bg-cyan-500/50"
+                onClick={() => createProject({ name: newProject })}
+              >
+                Add Project
+              </button>
+            </form>
+          </div>
         </div>
-        <form className="flex gap-2 xs:max-sm:justify-between">
-          <input
-            placeholder="Enter New Project"
-            className="flex border px-2 py-1"
-            type="text"
-            value={newProject}
-            onChange={(e) => setNewProject(e.target.value)}
-          />
-          <button
-            className="flex w-32 rounded-lg border border-cyan-600 px-2 py-1 text-center font-semibold text-gray-500 no-underline transition hover:bg-cyan-500/50"
-            onClick={() => createProject({ name: newProject })}
-          >
-            Add Project
-          </button>
-        </form>
+      </div>
+      <div>
+        <Image
+          className="flex items-center justify-center xs:max-md:hidden"
+          src="/favicon.ico"
+          width={50}
+          height={50}
+          alt="Logo"
+        />
       </div>
       <div className="flex items-center justify-between gap-1 px-2 xs:max-sm:w-full sm:flex-col">
         {sessionData && (
           <div className="text-md flex items-center gap-2 text-gray-500">
             <Image
               className="rounded-full"
-              src={sessionData.user?.image || "/favicon.png"}
+              src={sessionData.user?.image || "/favicon.cio"}
               width={25}
               height={25}
               alt="Profile Pic"
